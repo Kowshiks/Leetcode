bf914a9facdc1594f3011f6f557418ff6d820675
Kowshik Selvam <31544915+Kowshiks@users.noreply.github.com>
2023-01-06 19:23:44 +0530

    Update PermutationInString.py

--- PermutationInString.py
+++ PermutationInString.py
@@ -6,33 +6,26 @@ class Solution:
         for i in s1:
             
             if i not in cache.keys():
-                
                 cache[i] = 1
             
             else:
-                
                 cache[i]+=1
                 
         total = 0
-        
         count = 0
         
         for i in s1:
             
             total += ord(i)
-            
             count += 1
         
         l = 0
-        
         val = 0
-        
         tmp_cache = {}
         
         for r in range(len(s2)):
             
             if s2[r] not in tmp_cache.keys():
-                
                 tmp_cache[s2[r]] = 1
             
             else:
@@ -44,11 +37,9 @@ class Solution:
             if val > total or (r-l+1) > count:
                 
                 val = val - ord(s2[l])
-                
                 tmp_cache[s2[l]] = tmp_cache[s2[l]] - 1
                 
                 if tmp_cache[s2[l]] == 0:
-                    
                     del tmp_cache[s2[l]]
                 
                 l+=1
@@ -56,15 +47,12 @@ class Solution:
             if val == total and (r-l+1) == count:
                                 
                 if cache == tmp_cache:
-            
                     return True
             
                 else:
                     
                     val = val - ord(s2[l])
-                    
                     tmp_cache[s2[l]] = tmp_cache[s2[l]] - 1
-                
                     if tmp_cache[s2[l]] == 0:
                     
                         del tmp_cache[s2[l]]
